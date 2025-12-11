c3fb4378f5e829702a609e16a5da33e3cf46796d
MaxBenKre <maxi.kreutner@gmail.com>
2025-12-11 13:59:37 +0100

    update release

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ author = 'Maximilian Kreutner'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-release = '0.1.0'
+release = '0.1.1'
 
 extensions = [
     'sphinx.ext.autodoc',      # Core library to pull documentation from docstrings
