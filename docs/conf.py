0638fbed7b16a714bde49e10a45a8a450c440c12
MaxBenKre <maxi.kreutner@gmail.com>
2026-01-23 15:05:00 +0100

    testing themes

--- docs/conf.py
+++ docs/conf.py
@@ -54,5 +54,5 @@ root_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx-book-theme"
 html_static_path = ['_static']
