a686ffe54cc87c8d5461e025dcf73771f2bbc658
MaxBenKre <maxi.kreutner@gmail.com>
2026-02-27 11:11:03 +0100

    added dynamic versionning

--- src/qstn/__init__.py
+++ src/qstn/__init__.py
@@ -1,4 +1,15 @@
 """Public exports for main modules"""
+
+from importlib.metadata import PackageNotFoundError, version as package_version
+
 from . import prompt_builder, survey_manager
 
-__all__ = ["prompt_builder", "survey_manager"]
+try:
+    from ._version import __version__
+except Exception:
+    try:
+        __version__ = package_version("qstn")
+    except PackageNotFoundError:
+        __version__ = "0+unknown"
+
+__all__ = ["prompt_builder", "survey_manager", "__version__"]
---
+++ src/qstn/_version.py
@@ -0,0 +1,3 @@
+"""Fallback package version for local/editable usage."""
+
+__version__ = "0+unknown"
